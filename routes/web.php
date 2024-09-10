8e0beea8c5238430f65b6c135f7d0ef5c8720d0b
Khadim NDao <bayecode5991@gmail.com>
2024-09-10 13:46:14 +0000

    delete home root

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
